a96935a87c2a99f98341e65c82e845c51ac2db3f
kino6052 <kino6052@colorado.edu>
2018-12-06 16:31:10 -0700

    Initial Commit

---
+++ react-typescript/src/index.ts
@@ -0,0 +1,10 @@
+import * as _ from 'lodash';
+
+function component() {
+    let element = document.createElement('div');
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = 'Hello Webpack';
+    return element;
+};
+
+document.body.appendChild(component());
